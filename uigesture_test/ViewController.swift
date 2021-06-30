bd23f3503e97f303ba0f30fbe0130b96c830420a
seoyeon <hongssup0202@gmail.com>
2021-06-30 15:15:59 +0900

    Initial Commit

---
+++ uigesture_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uigesture_test
+//
+//  Created by SeoYeon Hong on 2021/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
